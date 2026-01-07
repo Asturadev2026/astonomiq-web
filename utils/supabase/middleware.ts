71b1ec64ec8e7af8a43981d498261f71540ce55c
Abhishek Srivastava <apple@apples-MacBook-Pro.local>
2026-01-07 17:40:04 +0530

    auth layer and supabase init

---
+++ utils/supabase/middleware.ts
@@ -0,0 +1,39 @@
+import { NextResponse, type NextRequest } from "next/server"
+import { createServerClient } from "@supabase/ssr"
+
+export async function updateSession(req: NextRequest) {
+    let res = NextResponse.next({
+        request: {
+            headers: req.headers,
+        },
+    })
+
+    const supabase = createServerClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
+        {
+            cookies: {
+                getAll() {
+                    return req.cookies.getAll()
+                },
+                setAll(cookiesToSet) {
+                    cookiesToSet.forEach(({ name, value, options }) => req.cookies.set(name, value))
+                    res = NextResponse.next({
+                        request: {
+                            headers: req.headers,
+                        },
+                    })
+                    cookiesToSet.forEach(({ name, value, options }) =>
+                        res.cookies.set(name, value, options)
+                    )
+                },
+            },
+        }
+    )
+
+    const { data: { user } } = await supabase.auth.getUser()
+
+    if (!user) return NextResponse.redirect(new URL("/login", req.url))
+
+    return res
+}
